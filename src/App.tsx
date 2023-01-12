d9156e63ad155854b0b3c6cd73d9d17624e23bd9
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 14:10:44 +0900

    import moduels

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import Auth from './components/Auth'
+import MainPage from './components/MainPage'
 import styles from './App.module.css'
 
 const App: React.FC = () => {
