82a0e54d20c23a9b40e3443715ae04501fd69698
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 14:48:57 +0900

    set states

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { useNavigate } from 'react-router-dom'
+import { FlipCameraAndroid } from '@material-ui/icons'
+import { CREATE_USER, GET_TOKEN, CREATE_PROFILE } from '../queries'
+import styles from './Auth.module.css'
 
 const Auth: React.FC = () => {
+  const navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLogin, setIsLogin] = useState(true)
+  const [createUser] = useMutation(CREATE_USER)
+  const [getToken] = useMutation(GET_TOKEN)
+  const [createProfile] = useMutation(CREATE_PROFILE)
+
   return <div>Auth</div>
 }
 
