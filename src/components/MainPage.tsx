4e6abbc579a5673036229ecc37b93a3c2cc0a65b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 16:33:43 +0900

    display followings and follower

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -30,13 +30,86 @@ const MainPage: React.FC = () => {
           {errorProfiles!.message}/{errorMyProfile!.message}
         </h3>
       )}
-      <ExitToApp
-        className={styles.mainPage__out}
-        onClick={() => {
-          localStorage.removeItem('token')
-          navigate('/')
-        }}
-      />
+      <Grid container>
+        <Grid item xs>
+          {dataMyProfile?.profile.userProf.username}
+        </Grid>
+        <Grid item xs>
+          <span className={styles.mainPage__title}>Follow system</span>
+        </Grid>
+        <Grid item xs>
+          <ExitToApp
+            className={styles.mainPage__out}
+            onClick={() => {
+              localStorage.removeItem('token')
+              navigate('/')
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={4}>
+          <h3>Following</h3>
+          <ul className={styles.mainPage__list}>
+            {dataMyProfile?.profile.followings.edges.map((node: any) => (
+              <li className={styles.mainPage__item} key={node.id}>
+                {node.username}
+              </li>
+            ))}
+          </ul>
+        </Grid>
+        <Grid item xs={4}>
+          <h3>ProfileList</h3>
+          <ul className={styles.mainPage__list}>
+            {dataProfiles.allProfiles.edges.map(
+              (node: any) =>
+                node.id !== dataMyProfile?.profile.id && (
+                  <li className={styles.mainPage__item} key={node.id}>
+                    {node.userProf.username}
+                    <button
+                      onClick={async () => {
+                        if (myFollowings?.includes(node.userProf.id)) {
+                          await updateProfile({
+                            variables: {
+                              id: dataMyProfile.profile.id,
+                              followings: myFollowings.filter(
+                                (followingId: any) =>
+                                  followingId !== node.userProf.id
+                              ),
+                            },
+                          })
+                        } else {
+                          await updateProfile({
+                            variables: {
+                              id: dataMyProfile.profile.id,
+                              followings: [...myFollowings, node.userProf.id],
+                            },
+                          })
+                        }
+                      }}
+                    >
+                      {myFollowings?.includes(node.userProf.id)
+                        ? 'unfollow'
+                        : 'follow'}
+                    </button>
+                  </li>
+                )
+            )}
+          </ul>
+        </Grid>
+        <Grid item xs={4}>
+          <h3>Followers</h3>
+          <ul className={styles.mainPage__list}>
+            {dataMyProfile?.profile.userProf.profilesFollowings.edges.map(
+              (node: any) => (
+                <li className={styles.mainPage__item} key={node.id}>
+                  {node.userProf.username}
+                </li>
+              )
+            )}
+          </ul>
+        </Grid>
+      </Grid>
     </div>
   )
 }
