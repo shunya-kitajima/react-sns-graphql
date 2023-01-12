384798bcce2551952f9c3bc48c9c33756dc92f3a
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 15:42:18 +0900

    set datas and error message

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,13 +1,35 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useQuery } from '@apollo/react-hooks'
+import { useMutation } from '@apollo/client'
+import { Grid } from '@material-ui/core'
 import ExitToApp from '@material-ui/icons/ExitToApp'
+import { UPDATE_PROFILE, GET_PROFILES, GET_MYPROFILE } from '../queries'
 import styles from './MainPage.module.css'
 
 const MainPage: React.FC = () => {
   const navigate = useNavigate()
+  const { data: dataMyProfile, error: errorMyProfile } = useQuery(
+    GET_MYPROFILE,
+    {
+      fetchPolicy: 'cache-and-network',
+    }
+  )
+  const { data: dataProfiles, error: errorProfiles } = useQuery(GET_PROFILES, {
+    fetchPolicy: 'cache-and-network',
+  })
+  const myFollowings = dataMyProfile?.profile.followings.edges.map(
+    (node: any) => node.id
+  )
+  const [updateProfile] = useMutation(UPDATE_PROFILE)
 
   return (
     <div className={styles.mainPage__root}>
+      {(errorMyProfile || errorProfiles) && (
+        <h3>
+          {errorProfiles!.message}/{errorMyProfile!.message}
+        </h3>
+      )}
       <ExitToApp
         className={styles.mainPage__out}
         onClick={() => {
