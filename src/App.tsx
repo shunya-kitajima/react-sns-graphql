0d19841951d14a78e35f9943e944afc24a11b6fa
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 14:06:45 +0900

    import module

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import styles from './App.module.css'
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
