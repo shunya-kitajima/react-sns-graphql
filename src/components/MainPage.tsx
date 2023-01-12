193989d2ad7fcb74a295bf18654110aff7874257
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 15:19:57 +0900

    import modules and display logout icon

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import ExitToApp from '@material-ui/icons/ExitToApp'
 import styles from './MainPage.module.css'
 
 const MainPage: React.FC = () => {
-  return <div>MainPage</div>
+  const navigate = useNavigate()
+
+  return (
+    <div className={styles.mainPage__root}>
+      <ExitToApp
+        className={styles.mainPage__out}
+        onClick={() => {
+          localStorage.removeItem('token')
+          navigate('/')
+        }}
+      />
+    </div>
+  )
 }
 
 export default MainPage
