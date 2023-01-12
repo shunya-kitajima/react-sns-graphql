969f211983b4d8b8fbee9f4a36a14138cdf58514
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 14:22:44 +0900

    set client and router

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,36 @@ import Auth from './components/Auth'
 import MainPage from './components/MainPage'
 import styles from './App.module.css'
 
+const httpLink = createHttpLink({
+  uri: 'http://127.0.0.1:8000/graphql/',
+})
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `JWT ${token}` : '',
+    },
+  }
+})
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
 const App: React.FC = () => {
-  return <div></div>
+  return (
+    <ApolloProvider client={client}>
+      <div className={styles.app__root}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/top" element={<MainPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
+  )
 }
 
 export default App
