e8d7815b2538697f9a5bbeb51644da548d786008
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2023-01-12 15:15:27 +0900

    import module

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import ExitToApp from '@material-ui/icons/ExitToApp'
+import styles from './MainPage.module.css'
 
 const MainPage: React.FC = () => {
   return <div>MainPage</div>
